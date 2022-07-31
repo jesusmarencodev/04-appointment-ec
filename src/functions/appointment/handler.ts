e4aa2168ee53c715f7eab0095055f8a850b726c5
Jesus Marenco <jesus.marenco@externos-cl.cencosud.com>
2022-07-31 12:33:57 -0500

    agregar eventBridge a la lambda de ecuador ya esta ok con pruebas manuales

--- src/functions/appointment/handler.ts
+++ src/functions/appointment/handler.ts
@@ -1,6 +1,7 @@
 
 export const appointmentHandler = async (event) => {
   console.log("estoy desde la lambda que ejecutara operaciones desde ecuador");
+  console.log(event);
 
   return {
     statusCode: 200,
